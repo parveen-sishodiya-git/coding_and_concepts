3a34d053a04c587627e216e35c9233f8efb7c47a
parveen_sishodiya_git <sishodiya.praveen@gmail.com>
2025-12-14 12:35:26 +0530

    testing workflow

--- src/main/java/com/sishodiya/practice/array/TestArray.java
+++ src/main/java/com/sishodiya/practice/array/TestArray.java
@@ -3,5 +3,6 @@ package main.java.com.sishodiya.practice.array;
 public class TestArray {
     public static void main(String[] args) {
         System.out.println("hi test method");
+        System.out.println("test edit");
     }
 }
